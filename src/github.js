4415c3c2f1ed6dbe3b2d0be85b2007f47e976ee3
Mauro Verrocchio <mauroverrocchio@gmail.com>
2015-06-27 12:01:35 +0200

    adds error handling

--- src/github.js
+++ src/github.js
@@ -1,10 +1,16 @@
 const githubBaseURL = 'https://api.github.com';
 
 let getUserReposListURL = (username) => {
+  if (!username) {
+    throw new Error('Username is undefined, null or an empty string');
+  }
   return `${githubBaseURL}/users/${encodeURIComponent(username)}/repos`;
 };
 
 let getUserRepoURL = (username, reponame) => {
+  if (!username || !reponame) {
+    throw new Error('Username or repository name is undefined, null or an empty string');
+  }
   return `${githubBaseURL}/repos/${encodeURIComponent(username)}/${encodeURIComponent(reponame)}`;
 };
 
--- test/github-test.js
+++ test/github-test.js
@@ -8,8 +8,36 @@ describe('github module', () => {
     expect(github.getUserReposListURL('maur/8i&no')).to.equal('https://api.github.com/users/maur%2F8i%26no/repos');
   });
 
+  it('should throw an error if the username is undefined, null or an empty string', () => {
+    let fn = github.getUserReposListURL.bind(github.getUserReposListURL, undefined);
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
+
+    fn = github.getUserReposListURL.bind(github.getUserReposListURL, null);
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
+
+    fn = github.getUserReposListURL.bind(github.getUserReposListURL, '');
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
+  });
+
   it('should generate the correct specific user\'s repository endpoint url using encodeURIComponent', () => {
     expect(github.getUserRepoURL('maur8ino', 'react-bem-mixin')).to.equal('https://api.github.com/repos/maur8ino/react-bem-mixin');
     expect(github.getUserRepoURL('maur/8i&no', 'rea/ct-bem-\mixin')).to.equal('https://api.github.com/repos/maur%2F8i%26no/rea%2Fct-bem-mixin');
+  });
+
+  it('should throw an error if either the username or the repository name is undefined, null or an empty string', () => {
+    let fn = github.getUserRepoURL.bind(github.getUserRepoURL, undefined, 'react-bem-mixin');
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
+
+    fn = github.getUserRepoURL.bind(github.getUserRepoURL, 'maur8ino', undefined);
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
+
+    fn = github.getUserRepoURL.bind(github.getUserRepoURL, undefined, undefined);
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw(/empty string/);
   });
 });
