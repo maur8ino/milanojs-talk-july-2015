45887c4fe27e99763f03bd0b8362c12636126c34
Mauro Verrocchio <mauroverrocchio@gmail.com>
2015-07-03 20:42:45 +0200

    adds encodeURICompoment to input username and reponame

--- src/github.js
+++ src/github.js
@@ -1,11 +1,11 @@
 const githubBaseURL = 'https://api.github.com';
 
 let getUserReposListURL = (username) => {
-  return `${githubBaseURL}/users/${username}/repos`;
+  return `${githubBaseURL}/users/${encodeURIComponent(username)}/repos`;
 };
 
 let getUserRepoURL = (username, reponame) => {
-  return `${githubBaseURL}/repos/${username}/${reponame}`;
+  return `${githubBaseURL}/repos/${encodeURIComponent(username)}/${encodeURIComponent(reponame)}`;
 };
 
 module.exports = {
--- test/github-test.js
+++ test/github-test.js
@@ -3,11 +3,13 @@ let expect = require('chai').expect;
 let github = require('../src/github');
 
 describe('github module', () => {
-  it('should generate the correct user\'s list of repositories endpoint url', () => {
+  it('should generate the correct user\'s list of repositories endpoint url using encodeURIComponent', () => {
     expect(github.getUserReposListURL('maur8ino')).to.equal('https://api.github.com/users/maur8ino/repos');
+    expect(github.getUserReposListURL('maur/8i&no')).to.equal('https://api.github.com/users/maur%2F8i%26no/repos');
   });
 
-  it('should generate the correct specific user\'s repository endpoint url', () => {
+  it('should generate the correct specific user\'s repository endpoint url using encodeURIComponent', () => {
     expect(github.getUserRepoURL('maur8ino', 'react-bem-mixin')).to.equal('https://api.github.com/repos/maur8ino/react-bem-mixin');
+    expect(github.getUserRepoURL('maur/8i&no', 'rea/ct-bem-\mixin')).to.equal('https://api.github.com/repos/maur%2F8i%26no/rea%2Fct-bem-mixin');
   });
 });
